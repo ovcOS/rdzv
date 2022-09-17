8acd30a1081357dfc38b734c1f00e9fd13e7bdb5
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-09-17 15:02:08 +0200

    Add viewport meta

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -8,6 +8,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
         <title>Home</title>
         <meta name="description" content="Meet at the most convenient place" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
       <main className={styles.main}>{children}</main>
