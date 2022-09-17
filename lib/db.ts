88c8ba4e4a774acb586de3fe0a52893d9b991d37
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-09-17 11:28:22 +0200

    Prettify files

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import clientPromise from "./mongodb";
+import clientPromise from './mongodb';
 
 if (!process.env.DB_NAME) {
   throw new Error('Please add your Mongo DB_NAME to .env.local');
@@ -8,11 +8,11 @@ const DB_NAME = process.env.DB_NAME as string;
 
 const COLLECTIONS = Object.freeze({
   ROOMS: 'rooms',
-})
+});
 
 export const getRoom = async (name: string): Promise<RoomProps | null> => {
   const client = await clientPromise;
   const collection = client.db(DB_NAME).collection<RoomProps>(COLLECTIONS.ROOMS);
   const room = await collection.findOne({ name });
   return room;
-}
+};
--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,13 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { getRoom } from '@/lib/db';
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = RoomProps | null
+type Data = RoomProps | null;
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const room = await getRoom('testRoom');
   res.status(200).json(room);
 }
