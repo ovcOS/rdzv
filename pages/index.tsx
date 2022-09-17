fb284ec4fc6792a04a7190fb756174637a875f05
xiao <sunxiaolive2016@outlook.com>
2022-09-17 13:52:09 +0200

    add two buttons to create or enter room

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,51 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Button from '@mui/material/Button';
-import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
+import Grid from '@mui/material/Grid';
+import { useState } from 'react';
+
+type Status = 'new' | 'existing';
+
+const ChooseStatus = ({ setStatus }: { setStatus: (arg: Status) => void }) => (
+  <>
+    <Grid item xs={6} style={{ textAlign: 'center' }}>
+      <Button variant="outlined" onClick={() => setStatus('new')}>
+        Create new room
+      </Button>
+    </Grid>
+    <Grid item xs={6} style={{ textAlign: 'center' }}>
+      <Button variant="outlined" onClick={() => setStatus('existing')}>
+        Enter a room
+      </Button>
+    </Grid>
+  </>
+);
+
+const NewRoomSettings = () => {
+  const roomName = 'testRoomNameXiaoLalala';
+  const hasRoomName = !!roomName;
+  return (
+    <>
+      <Button variant="outlined" onClick={() => undefined} disabled={!hasRoomName}>
+        Continue
+      </Button>
+    </>
+  );
+};
+const ExistingRoomSettings = () => <div>existing room fields</div>;
 
 const Home: NextPage = () => {
+  const [status, setStatus] = useState<Status | undefined>(undefined);
+
+  const isStatusKnown = !!status;
+  const isNewRoom = status === 'new';
+  const isExistingRoom = status === 'existing';
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>rdzv</title>
+        <title>Home</title>
         <meta name="description" content="Meet at the most convenient place" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
@@ -19,9 +54,11 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           welcome to <code>rdzv</code> 🤓
         </h1>
-        <Button variant="contained">Get started</Button>
-        <Switch defaultChecked />
-        <DeleteIcon />
+        <Grid container xs={4} style={{ marginTop: '20px' }}>
+          {!isStatusKnown && <ChooseStatus setStatus={setStatus} />}
+          {isNewRoom && <NewRoomSettings />}
+          {isExistingRoom && <ExistingRoomSettings />}
+        </Grid>
       </main>
     </div>
   );
