3f58a8f323032d9b76ce9c59965acf0982d19c40
xiao <sunxiaolive2016@outlook.com>
2022-09-17 16:43:26 +0200

    add home link

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import { Theme } from './theme';
+import HomeIcon from '@mui/icons-material/Home';
+import Link from 'next/link';
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <>
@@ -13,6 +15,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
           <meta name="viewport" content="initial-scale=1, width=device-width" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         </Head>
+        <Link href="/">
+          <HomeIcon color="inherit" aria-label="menu" sx={{ mr: 2 }} style={{ margin: '15px' }} />
+        </Link>
         <main className={styles.main}>{children}</main>
       </div>
     </Theme>
