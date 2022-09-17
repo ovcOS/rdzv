92c8cd26e148cc6451cf962142186c5209dbe3fb
Oriol Vidal Cortes <ovidalcortes@gmail.com>
2022-09-17 18:24:14 +0200

    support multiple routes, compute middle point, add test data for existing origins

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState, Dispatch, SetStateAction } from 'react';
-import {
-  GoogleMap,
-  LoadScript,
-  Marker,
-  DirectionsService,
-  DirectionsRenderer,
-  Autocomplete,
-} from '@react-google-maps/api';
+import { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer, Autocomplete } from '@react-google-maps/api';
+import { Button } from '@mui/material';
+import { useMemo } from 'react';
 
 type SetState<T> = Dispatch<SetStateAction<T>>;
 type PlaceResult = google.maps.places.PlaceResult;
 type AutocompleteProps = google.maps.places.Autocomplete;
 type DirectionsResult = google.maps.DirectionsResult;
 type DirectionsStatus = google.maps.DirectionsStatus;
-type Position = { lat: number; lng: number };
 
 const LIBRARIES: ['places'] = ['places'];
 
@@ -42,7 +36,7 @@ const containerStyle = {
   height: '500px',
 };
 
-const getPositionFromPlaceResult = (placeResult: PlaceResult): Position | null => {
+const getLocationFromPlaceResult = (placeResult: PlaceResult): LocationProps | null => {
   const lat = placeResult.geometry?.location?.lat();
   const lng = placeResult.geometry?.location?.lng();
   if (!lat || !lng) return null;
@@ -54,77 +48,142 @@ const onLoad = (autocompleteInstance: AutocompleteProps, setAutocomplete: SetSta
   setAutocomplete(autocompleteInstance);
 };
 
-const onPlaceChanged = (autocomplete: AutocompleteProps | null, setSelectedDestination: SetState<Position | null>) => {
+const onPlaceChanged = (autocomplete: AutocompleteProps | null, setSelectedOrigin: SetState<LocationProps | null>) => {
   if (!autocomplete) return;
   const place = autocomplete.getPlace();
-  const position = getPositionFromPlaceResult(place);
-  setSelectedDestination(position);
+  const location = getLocationFromPlaceResult(place);
+  setSelectedOrigin(location);
 };
 
-export const Map = React.memo(() => {
-  const [center, setCenter] = useState(defaultPosition);
-  const [origin, setSelectedOrigin] = useState(defaultPosition);
-  const [destination, setSelectedDestination] = useState(null as Position | null);
-  const [autocomplete, setAutocomplete] = useState(null as AutocompleteProps | null);
-  // for now only using transit as travel mode
-  const [travelMode, setTravelMode] = useState('TRANSIT' as TravelMode);
-  const [directionsResult, setDirectionsResult] = useState(null as DirectionsResult | null);
+const setInitialCenter = (setCenter: SetState<LocationProps>, initialLocation?: LocationProps) => {
+  if (initialLocation) {
+    setCenter(initialLocation);
+  }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        const currentPosition = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        setCenter(currentPosition);
+      },
+      () => {
+        console.log('error!!');
+      }
+    );
+  } else {
+    console.error('geolocation not supported');
+  }
+};
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position: GeolocationPosition) => {
-          const currentPosition = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          setCenter(currentPosition);
-          setSelectedOrigin(currentPosition);
-        },
-        () => {
-          console.log('error!!');
-        }
-      );
-    } else {
-      console.error('geolocation not supported');
-    }
-  }, []);
+const setCurrentLocationAsOrigin = (stateSetter: SetState<LocationProps | null>) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        const currentPosition = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        stateSetter(currentPosition);
+      },
+      () => {
+        console.log('error!!');
+      }
+    );
+  } else {
+    console.error('geolocation not supported');
+  }
+};
 
-  const directionsServiceCallback = (result: DirectionsResult | null, status: DirectionsStatus) => {
-    if (!result) return;
-    if (status === 'OK') {
-      setDirectionsResult(result);
-    } else {
-      console.error('uh oh, something went wrong', { result });
-    }
-  };
-
-  return (
-    <LoadScript libraries={LIBRARIES} googleMapsApiKey="AIzaSyDEq9i1TN3va9qFrydwl-0TfbpLmhb_FxQ">
-      <div className="d-block" style={{ height: '50px' }}>
-        <Autocomplete
-          onLoad={(instance) => onLoad(instance, setAutocomplete)}
-          onPlaceChanged={() => onPlaceChanged(autocomplete, setSelectedDestination)}
-        >
-          <input type="text" placeholder="Where do you want to go?" style={autoCompleteStyle} />
-        </Autocomplete>
-      </div>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={ZOOM}>
-        {!!(origin && destination && !directionsResult) && (
-          <DirectionsService
-            options={{ destination: destination, origin, travelMode }}
-            callback={(result, status) => directionsServiceCallback(result, status)}
-          />
-        )}
-        {!!directionsResult && (
-          <DirectionsRenderer
-            options={{
-              directions: directionsResult,
-            }}
-          />
-        )}
-        <Marker position={origin} onPositionChanged={() => console.log('marker position changed', origin)} visible />
-      </GoogleMap>
-    </LoadScript>
+const mapOriginsToFinalDestination = (
+  existingOrigins: LocationProps[],
+  origin: LocationProps | null
+): { origin: LocationProps; destination: LocationProps }[] => {
+  const allOrigins = [...existingOrigins, ...(origin ? [origin] : [])];
+  const finalDestination = allOrigins.reduce(
+    (res, location, index, original) => {
+      const { length } = original;
+      const { lat, lng } = location;
+      const { lat: existingLat, lng: existingLng } = res;
+      if (index + 1 === length) {
+        return { lat: (existingLat + lat) / length, lng: (existingLng + lng) / length };
+      }
+      return { lat: existingLat + lat, lng: existingLng + lng };
+    },
+    { lat: 0, lng: 0 }
   );
-});
+  return allOrigins.map((v) => ({ origin: v, destination: finalDestination }));
+};
+
+export const Map = React.memo(
+  ({
+    existingOrigins = [],
+    existingMeetingLocation,
+  }: {
+    existingMeetingLocation?: LocationProps;
+    existingOrigins?: LocationProps[];
+  }) => {
+    const [center, setCenter] = useState(defaultPosition);
+    const [origin, setSelectedOrigin] = useState(null as LocationProps | null);
+    const [autocomplete, setAutocomplete] = useState(null as AutocompleteProps | null);
+    // for now only using transit as travel mode
+    const [travelMode, setTravelMode] = useState('TRANSIT' as TransportationMode);
+    const [directionsResult, setDirectionsResult] = useState([] as DirectionsResult[]);
+
+    useEffect(() => {
+      setInitialCenter(setCenter, existingMeetingLocation);
+    }, [existingMeetingLocation]);
+
+    const directionsServiceCallback = (result: DirectionsResult | null, status: DirectionsStatus) => {
+      if (!result) return;
+      if (status === 'OK') {
+        setDirectionsResult((existingResults) => [...existingResults, result]);
+      } else {
+        console.error('uh oh, something went wrong', { result });
+      }
+    };
+    // if the first origin is set by the current user, display a message telling them
+    // a route will be defined once more people define their origin
+
+    const originsToFinalDestination = useMemo(
+      () => mapOriginsToFinalDestination(existingOrigins, origin),
+      [existingOrigins, origin]
+    );
+    console.log({ origin, originsToFinalDestination, directionsResult });
+
+    return (
+      <LoadScript libraries={LIBRARIES} googleMapsApiKey="AIzaSyDEq9i1TN3va9qFrydwl-0TfbpLmhb_FxQ">
+        <div className="d-flex flex-column" style={{ minHeight: '120px' }}>
+          <div>Where are you coming from?</div>
+          <div className="d-flex">
+            <Button color="primary" onClick={() => setCurrentLocationAsOrigin(setSelectedOrigin)}>
+              Current location
+            </Button>
+            or
+            <Autocomplete
+              onLoad={(instance) => onLoad(instance, setAutocomplete)}
+              onPlaceChanged={() => onPlaceChanged(autocomplete, setSelectedOrigin)}
+            >
+              <input type="text" placeholder="Type a location" style={autoCompleteStyle} />
+            </Autocomplete>
+          </div>
+        </div>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={ZOOM}>
+          {!!(originsToFinalDestination.length && !directionsResult.length) &&
+            originsToFinalDestination.map(({ origin, destination }, i) => (
+              <DirectionsService
+                key={`${origin.lat}-${i}`}
+                options={{ destination, origin, travelMode }}
+                callback={directionsServiceCallback}
+              />
+            ))}
+          {!!directionsResult.length &&
+            directionsResult.map((directions, i) => (
+              <DirectionsRenderer key={`directions-renderer-${i}`} options={{ directions }} />
+            ))}
+        </GoogleMap>
+      </LoadScript>
+    );
+  }
+);
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,5 +1,10 @@
 import { Map } from '../components';
 
-const MapPage = () => <Map />;
+const existingOriginsTest = [
+  { lat: 47.5595986, lng: 7.5885761 }, // basel
+  { lat: 45.4642035, lng: 9.189982 }, // milano
+];
+
+const MapPage = () => <Map existingOrigins={existingOriginsTest} />;
 
 export default MapPage;
