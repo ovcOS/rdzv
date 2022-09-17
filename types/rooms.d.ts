3371791e6315712ef9daa3fa7afd96bbf26e8e90
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-09-17 13:22:13 +0200

    Add initial room types

--- types/rooms.d.ts
+++ types/rooms.d.ts
@@ -1,4 +1,24 @@
+type Id = string;
+
 declare type RoomProps = {
-  _id: string;
+  _id: Id;
   name: string;
+  slug: string;
+  participants: ParticipantProps[];
+  transportationMode: TransportationMode;
+  meetingLocation?: LocationProps;
+};
+
+declare type TransportationMode = 'public' | 'walking' | 'car' | 'bike';
+
+declare type LocationProps = {
+  lat: number;
+  lng: number;
+};
+
+declare type ParticipantProps = {
+  _id: Id;
+  name: string;
+  location: LocationProps;
+  route?: ParticipantRouteProps;
 };
