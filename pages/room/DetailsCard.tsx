f893961516a8591548d5accaa602b46d20e9dc4b
xiao <sunxiaolive2016@outlook.com>
2022-09-17 21:33:11 +0200

    improve details card

--- pages/room/DetailsCard.tsx
+++ pages/room/DetailsCard.tsx
@@ -1,18 +1,63 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Typography, Avatar, Paper, Grid } from '@mui/material';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+const stringToColor = (string: string) => {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+};
+
+const stringAvatar = (name: string) => {
+  const nameSplits = name.split(' ');
+  const isOneStringName = nameSplits.length === 1;
+  const firstInitial = name.split(' ')[0][0];
+  const secondInitial = isOneStringName ? undefined : name.split(' ')[1];
+  const initials = isOneStringName ? firstInitial : `${firstInitial}${secondInitial}`;
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: initials,
+  };
+};
 
 export const DetailsCard = ({ room, participantId }: { room: RoomProps; participantId: Id }) => (
-  <Card style={{ marginTop: '150px' }}>
+  <Card style={{ margin: '10px' }}>
     <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-        Room: {room.name}
+      <Typography gutterBottom variant="h5" component="div" style={{ marginBottom: '20px', textAlign: 'center' }}>
+        🏡 Room {room.name}
       </Typography>
-      Participants
-      {room.participants.map((v, index) => {
-        const isParticipant = v.id === participantId;
+      {room.participants.map((v, i) => {
+        const { id, name, location } = v;
+        const { lng, lat } = location;
+        const isParticipant = id === participantId;
         return (
-          <Typography key={index} variant="body2" color="text.secondary">
-            {v.name} - {JSON.stringify(v.location)} {isParticipant ? '<-' : ''}
-          </Typography>
+          <Paper key={i} variant="outlined">
+            <Grid container wrap="nowrap">
+              <Avatar {...stringAvatar(name)} style={{ marginRight: '5px' }} /> {v.name}
+            </Grid>
+            <Grid container wrap="nowrap">
+              <Grid xs={11}>
+                <LocationOnIcon /> {lat} {lng}
+              </Grid>
+              <Grid xs={1}>{isParticipant ? <CheckBoxIcon /> : ''}</Grid>
+            </Grid>
+          </Paper>
         );
       })}
     </CardContent>
