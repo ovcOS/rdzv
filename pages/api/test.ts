7254233f94a45e221ed7c87693bed9bd219b1ea4
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-09-17 11:01:47 +0200

    Add db test api endpoint

---
+++ pages/api/test.ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getRoom } from '@/lib/db';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = RoomProps | null
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const room = await getRoom('testRoom');
+  res.status(200).json(room);
+}
